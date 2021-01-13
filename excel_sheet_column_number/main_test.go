0640c7e2397103ce114a6c1b6006bb3903749c73
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-01-14 08:27:43 +0900

    Excel Sheet Column Number

---
+++ excel_sheet_column_number/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func Test_titleToNumber(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "titleToNumber",
+			args: args{
+				s: "CA",
+			},
+			want: 79,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := titleToNumber(tt.args.s); got != tt.want {
+				t.Errorf("titleToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
