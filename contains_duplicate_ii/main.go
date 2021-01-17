4cfef2a67e301f6c514e12639d170668f8eb08dc
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-01-17 09:35:55 +0900

    Contains Duplicate II

--- contains_duplicate_ii/main.go
+++ contains_duplicate_ii/main.go
@@ -1,6 +1,19 @@
 package main
 
 func containsNearbyDuplicate(nums []int, k int) bool {
+	m := make(map[int]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		num := nums[i]
+		index, ok := m[num]
+		if ok && i-index <= k {
+			return true
+		}
+		m[num] = i
+	}
+	return false
+}
+
+func containsNearbyDuplicate2(nums []int, k int) bool {
 	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j < i+1+k; j++ {
 			if j == len(nums) {
