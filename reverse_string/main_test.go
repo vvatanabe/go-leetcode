363922e67f740b325b2885cb758c605c7e2ac8e6
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-01-26 06:20:03 +0900

    Reverse String

---
+++ reverse_string/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			args: args{
+				s: []byte("hello"),
+			},
+			want: []byte("olleh"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString(tt.args.s)
+			if reflect.DeepEqual(tt.args.s, tt.want) {
+				t.Errorf("reverseString() = %v, want %v", tt.args.s, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseString2(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			args: args{
+				s: []byte("hello"),
+			},
+			want: []byte("olleh"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString2(tt.args.s)
+			if reflect.DeepEqual(tt.args.s, tt.want) {
+				t.Errorf("reverseString2() = %v, want %v", tt.args.s, tt.want)
+			}
+		})
+	}
+}
