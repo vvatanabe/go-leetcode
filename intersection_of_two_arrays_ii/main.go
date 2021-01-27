39cadf69ea379e2d57de4fe7bb5b0dd479695d50
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-01-28 07:26:48 +0900

    fix gocritic:assignOp in Intersection of Two Arrays II

--- intersection_of_two_arrays_ii/main.go
+++ intersection_of_two_arrays_ii/main.go
@@ -3,14 +3,14 @@ package main
 func intersect(nums1 []int, nums2 []int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(nums1); i++ {
-		m[nums1[i]] = m[nums1[i]] + 1
+		m[nums1[i]]++
 	}
 	var out []int
 	for i := 0; i < len(nums2); i++ {
 		cnt, ok := m[nums2[i]]
 		if ok && cnt > 0 {
 			out = append(out, nums2[i])
-			m[nums2[i]] = m[nums2[i]] - 1
+			m[nums2[i]]--
 		}
 	}
 	return out
