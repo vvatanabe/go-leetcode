7a833c1eff0ff95c4a87f531a2beefe9100e8521
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-02-17 00:33:10 +0900

    Detect Capital

--- problemset/detect_capital/main.go
+++ problemset/detect_capital/main.go
@@ -1,20 +1,25 @@
 package main
 
-// lower 97 - 122
-// upper 65 - 90
 func detectCapitalUse(word string) bool {
 	runes := []rune(word)
-	allLow := runes[0] >= 97 && runes[0] <= 122
-	if allLow && len(runes) == 2 {
-		return runes[1] >= 97 && runes[1] <= 122
+	if len(runes) == 2 {
+		return !isLower(runes[0]) || isLower(runes[1])
 	}
+	allLower := isLower(runes[0])
 	for i := 1; i < len(runes)-1; i++ {
-		if allLow && (!(runes[i] >= 97 && runes[i] <= 122) || !(runes[i+1] >= 97 && runes[i+1] <= 122)) {
-			return false
-		}
-		if !allLow && (runes[i] >= 97 && runes[i] <= 122) != (runes[i+1] >= 97 && runes[i+1] <= 122) {
-			return false
+		if allLower {
+			if !isLower(runes[i]) || !isLower(runes[i+1]) {
+				return false
+			}
+		} else {
+			if isLower(runes[i]) != isLower(runes[i+1]) {
+				return false
+			}
 		}
 	}
 	return true
+}
+
+func isLower(r rune) bool {
+	return r >= 'a' && r <= 'z'
 }
